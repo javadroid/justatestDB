b07f896a62ba2fbb18e5efd13102054f2d41206b
Nathan-Nsa <nathan.codedev@gmail.com>
2023-04-06 11:58:36 +0100

    Style Guidelines

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-      <h1 className="text-3xl font-bold underline text-rose-600">
+      <h1 className="text-yellow-600 font-bold underline ">
         Hello world This is the start
       </h1>
       </main>
