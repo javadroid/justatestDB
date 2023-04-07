492dd783b354bb7ec43ef191f7ea799a12945a80
Nathan-Nsa <nathan.codedev@gmail.com>
2023-04-07 10:01:57 +0100

    Done the Navbar and Footer(partially)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -14,11 +13,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-      <h1 className="text-yellow-600 font-bold underline ">
-        Hello world This is the start
-      </h1>
-      </main>
+      <main></main>
     </>
-  )
+  );
 }
