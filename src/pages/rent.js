558245ef64e6af7102ca4b82565df9f0ba610660
Oluwatoyin <oluwatoyinjolaoso001@gmail.com>
2023-04-13 10:40:16 -0700

    added rent page

--- src/pages/rent.js
+++ src/pages/rent.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Image from 'next/image'
+import chatting from '../assets/images/chatting.svg'
 import device from '../assets/images/Device.webp'
 import device2 from '../assets/images/pic_en.webp'
 import Estonia from '../assets/flags/Estonia.svg';
 import Cyprus from '../assets/flags/Cyprus.svg';
+import Link from 'next/link';
+import { Icon } from '@iconify/react';
 
 const Rent = () => {
   return (
@@ -229,6 +232,139 @@ const Rent = () => {
           </form>
         </div>
       </section>
+
+      {/* sign up */}
+      <section className="bg-color-primary_black">
+          <div className="max-w-full mx-auto py-8 px-4 lg:py-14">
+          <div className="max-w-sm m-auto lg:max-w-4xl xl:max-w-6xl">
+            <h3 className="font-bold text-2xl text-center my-4 text-color-white md:text-4xl">
+              Sign Up
+            </h3>
+            <form className="flex flex-col space-y-1 items-center justify-between mb-2 lg:flex-row lg:space-x-4 lg:space-y-0">
+              <input type="email" placeholder="Enter your email address" className="w-full border border-color-primary_black px-4 py-3 focus:outline-dashed focus:border-color-primary_black text-sm text-color-primary_black placeholder-color-primary bg-color-white md:text-lg lg:w-1/3" />
+              <input type="password" placeholder="Create password" className="w-full border border-color-primary_black px-4  py-3 focus:outline-dashed focus:border-color-primary_black text-sm text-color-primary_black placeholder-color-primary bg-color-white md:text-lg lg:w-1/3" />
+              <button className="w-full bg-color-primary text-color-white font-bold rounded-xl py-1 shadow-[0_4px_15px_rgba(231,237,250,.25)] md:text-lg md:py-4 lg:text-xl lg:py-4 lg:w-1/3">Sign up</button>
+            </form>
+          </div>
+          <div className="flex flex-col items-center justify-between m-auto space-y-4">
+            <p className="text-center text-color-text_light font-bold text-sm md:text-base lg:text-xl">
+              Or log in with your social network
+            </p>
+            <div className="flex space-x-4 items-center justify-center">
+              <Link href="/github"  className="w-1/4">
+                  <Icon icon="logos:github-octocat" className="w-full text-5xl transition duration-500 ease-in-out hover:opacity-75 md:text-6xl" />
+              </Link>
+              <Link href="/facebook"  className="w-1/4 text-center">
+                  <Icon icon="teenyicons:facebook-solid" className="w-full text-4xl text-[#4267b2] transition duration-500 ease-in-out hover:opacity-75 md:5xl" />
+              </Link>
+              <Link href="/twitter">
+                  <Icon icon="bi:twitter" className=" w-full text-4xl text-[#03a9f4] transition duration-500 ease-in-out hover:opacity-75 md:5xl" />
+              </Link>
+              <Link href="/google">
+                  <Icon icon="cib:google" className="w-full text-4xl text-[#d93025] transition duration-500 ease-in-out hover:opacity-75 md:text-5xl" />
+              </Link>
+            </div>
+            <div>
+              <Link href="/telegram">
+                <button className="flex items-center justify-center space-x-4 text-center text-color-white bg-[#54a9eb] px-4 py-2 rounded-md transition duration-500 ease-in-out hover:opacity-75">
+                  <Icon icon="akar-icons:telegram-fill" className="text-3xl" />
+                  <span className="md:text-base">Log in with Telegram</span>
+                </button>
+              </Link>
+            </div>
+          </div>
+          </div>
+      </section>
+
+
+      {/* You can buy or rent one */}
+
+
+      {/* About */}
+      <section className="bg-color-bg_primary-500">
+          <div className="container px-4 py-2">
+            <div className="max-w-2xl m-auto py-14 text-justify md:text-xl">
+              <p className="my-4">
+                &quot;SMS-Man&quot; gives you the opportunity to rent a virtual number to receive SMS. The leased number can be used for registration on almost any platform that requires a mobile number for registration.
+              </p>
+              <p className="my-4">
+                Many online services require you to confirm your phone number via SMS to register. This means that you can&apos;t register an additional account in your favorite messenger or social network without a phone number. But to apply for a new SIM card, you have to come to a cell phone shop, give your passport details and pay a lot of money...... Tremendous.
+              </p>
+              <p className="my-4">
+                If you rent a number on our service, you will be able to receive SMS on the rented number during the entire rental period. This will allow you to register an unlimited number of accounts for WhatsApp, Telegram, Gmail, Facebook, Tinder and any other services that require SMS verification!
+              </p>
+              <p className="my-4">
+                Number rental on the service &quot;SMS-Man&quot; has many advantages, which you can find below: <br />
+                - A rented number will help you preserve your anonymity in social networks and messengers. <br />
+                 
+                - You will finally be able to register as many social network accounts as you want <br />
+                - Renting a number will cost you much less than getting a new SIM-card. Prices SMS-man is one of the lowest. <br />
+                - You do not need to have special skills to rent a number. You can do it in a couple of minutes.<br />
+              </p>
+              <p className="my-4">You can rent a number right now!</p>
+            </div>
+            <div></div>
+          </div>
+          <div>
+            <div className="container py-12">
+              <div className="flex items-end justify-around lg:space-x-8">
+                <Image src={chatting} alt="texting" className="hidden w-1/3 lg:block" />
+                <div className="w-full px-10 pb-12 bg-color-white shadow-[0px_4px_15px_rgba(37,39,86,0.15)] md:rounded-3xl md:w-9/12 lg:w-2/4">
+                  <div className="max-w-xs m-auto">
+                    <h3 className="font-bold text-h2Size text-center my-4">
+                      Sign Up
+                    </h3>
+                    <form className="flex flex-col space-y-4 items-center justify-between">
+                      <input type="email" placeholder="Enter your email" className="w-full border border-color-primary_black px-4 py-3 rounded-lg focus:outline-dashed focus:border-color-primary_black text-xs text-color-primary_black md:text-lg" />
+                      <input type="password" placeholder="Create password" className="w-full border border-color-primary_black px-4  py-3 rounded-lg focus:outline-dashed focus:border-color-primary_black text-xs text-color-primary_black md:text-lg" />
+                      <button className="w-full bg-color-primary text-color-white font-bold rounded-3xl py-3 md:text-lg lg:text-xl lg:py-4">Sign up</button>
+                    </form>
+                    <p className="text-center my-4 text-color-text_light text-sm md:text-base">
+                      Already have an account? <Link href="/signup" className="text-color-primary font-bold hover:underline">Sign in.</Link>
+                    </p>
+                  </div>
+                  <div className="flex flex-col items-center justify-between m-auto space-y-4">
+                    <p className="text-center text-color-text_light font-bold text-sm md:text-base lg:text-xl">
+                      Or log in with your social network
+                    </p>
+                    <div className="flex space-x-4 items-center justify-center">
+                      <Link href="/github"  className="w-1/4">
+                          <Icon icon="logos:github-octocat" className="w-full text-5xl transition duration-500 ease-in-out hover:opacity-75 md:text-6xl" />
+                      </Link>
+                      <Link href="/facebook"  className="w-1/4 text-center">
+                          <Icon icon="teenyicons:facebook-solid" className="w-full text-4xl text-[#4267b2] transition duration-500 ease-in-out hover:opacity-75 md:text-5xl" />
+                      </Link>
+                      <Link href="/twitter">
+                          <Icon icon="bi:twitter" className=" w-full text-4xl text-[#03a9f4] transition duration-500 ease-in-out hover:opacity-75 md:text-5xl" />
+                      </Link>
+                      <Link href="/google">
+                          <Icon icon="cib:google" className="w-full text-4xl text-[#d93025] transition duration-500 ease-in-out hover:opacity-75 md:text-5xl" />
+                      </Link>
+                    </div>
+                    <div>
+                      <Link href="/telegram">
+                        <button className="flex items-center justify-center space-x-4 text-center text-color-white bg-[#54a9eb] px-4 py-2 rounded-lg transition duration-500 ease-in-out hover:opacity-75">
+                          <Icon icon="akar-icons:telegram-fill" className="text-3xl" />
+                          <span className="md:text-base">Log in with Telegram</span>
+                        </button>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+      </section>
+
+
+      {/* Advantages */}
+      <section>
+        <div className="container">
+         
+        </div>
+      </section>
+
+
     </>
   )
 }
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,7 +1,62 @@
 import React from 'react'
+import Link from 'next/link';
+import { Icon } from '@iconify/react';
 
-export default function signup() {
+const Signup = () => {
   return (
-    <div>signup</div>
+    <section id='signup' className="bg-color-bg_light">
+      <div className='container mx-auto py-6'>
+        <div className=" mx-4 px-6 pt-4 pb-12 bg-color-white rounded-2xl shadow-[0px_4px_15px_rgba(37,39,86,0.15)] md:rounded-3xl md:max-w-md md:m-auto lg:max-w-xl">
+          <div className="max-w-xs m-auto">
+            <h3 className="font-bold text-h2Size text-center my-4">
+              Log in
+            </h3>
+            <form className="flex flex-col space-y-4 items-center justify-items-center">
+              <input type="email" placeholder="Enter email" className="w-full border border-color-primary_black px-4 py-3 rounded-lg focus:outline-dashed focus:border-color-primary_black text-xs text-color-primary_black md:text-lg" />
+              <input type="password" placeholder="Enter password" className="w-full border border-color-primary_black px-4  py-3 rounded-lg focus:outline-dashed focus:border-color-primary_black text-xs text-color-primary_black md:text-lg" />
+              <input type="password" placeholder="Confirm password" className="w-full border border-color-primary_black px-4  py-3 rounded-lg focus:outline-dashed focus:border-color-primary_black text-xs text-color-primary_black md:text-lg" />
+              <button className="w-full bg-color-primary text-color-white font-bold rounded-3xl py-3 md:text-lg lg:text-xl lg:py-4">Log in</button>
+              <span>
+                <Link href="/" className="text-color-primary font-bold hover:underline">
+                  Forgot Password?
+                </Link>
+              </span>
+            </form>
+            <p className="text-center my-4 text-color-text_light text-sm md:text-base">
+              Don&apos;t have an account yet? <Link href="/signup" className="text-color-primary font-bold hover:underline">Sign in.</Link>
+            </p>
+          </div>
+          <div className="flex flex-col items-center justify-between m-auto space-y-4">
+            <p className="text-center text-color-text_light font-bold text-sm md:text-base lg:text-xl">
+              Or log in with your social network
+            </p>
+            <div className="flex space-x-4 items-center justify-center">
+              <Link href="/github"  className="w-1/4">
+                  <Icon icon="logos:github-octocat" className="w-full text-6xl transition duration-500 ease-in-out hover:opacity-75" />
+              </Link>
+              <Link href="/facebook"  className="w-1/4 text-center">
+                  <Icon icon="teenyicons:facebook-solid" className="w-full text-5xl text-[#4267b2] transition duration-500 ease-in-out hover:opacity-75" />
+              </Link>
+              <Link href="/twitter">
+                  <Icon icon="bi:twitter" className=" w-full text-5xl text-[#03a9f4] transition duration-500 ease-in-out hover:opacity-75" />
+              </Link>
+              <Link href="/google">
+                  <Icon icon="cib:google" className="w-full text-5xl text-[#d93025] transition duration-500 ease-in-out hover:opacity-75" />
+              </Link>
+            </div>
+            <div>
+              <Link href="/telegram">
+                <button className="flex items-center justify-center space-x-4 text-center text-color-white bg-[#54a9eb] px-4 py-2 rounded-lg transition duration-500 ease-in-out hover:opacity-75">
+                  <Icon icon="akar-icons:telegram-fill" className="text-3xl" />
+                  <span className="md:text-base">Log in with Telegram</span>
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
+
+export default Signup;
