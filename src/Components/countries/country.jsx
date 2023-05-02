cdc3aa4a229d8fa4a15349a745372ec5b2353ce4
Oluwatoyin <oluwatoyinjolaoso001@gmail.com>
2023-05-02 10:44:06 +0100

    completed user rent page

--- src/Components/countries/country.jsx
+++ src/Components/countries/country.jsx
@@ -8,7 +8,7 @@ const Country = () => {
       <tbody>
         {countries.map((country, index) =>(
         <tr key={index} className="bg-color-white hover:bg-color-bg_primary-500 active:bg-color-bg_primary-500 cursor-pointer  rounded-2xl">
-          <td className="flex items-center justify-start px-6 py-1">
+          <td className="flex items-center justify-start py-1 w-full">
             <span>
               <Image
                 src={country.flag}
@@ -16,10 +16,10 @@ const Country = () => {
                 className="mr-2 flex w-8 items-center"
               />
             </span>
-            <span className="text-sm font-medium md:text-base">{country.name}</span>
+            <span className="text-xs font-medium md:text-base">{country.name}</span>
           </td>
           <td>
-            <span className="text-base text-gray-500">
+            <span className="text-xs text-gray-500">
               {country.value}
             </span>
           </td>
