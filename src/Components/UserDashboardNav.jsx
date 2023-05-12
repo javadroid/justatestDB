755cfff7a2836d110a6b50796e55955e95354916
Nathan-Nsa <nathan.codedev@gmail.com>
2023-05-12 18:47:04 +0100

    Modal Fixes

--- src/Components/UserDashboardNav.jsx
+++ src/Components/UserDashboardNav.jsx
@@ -13,11 +13,11 @@ const UserDashboardNav = () => {
       <div className="flex flex-grow items-center justify-end lg:justify-between">
         <div className="hidden lg:flex lg:items-center lg:gap-11">
           <Image src={Logo} alt="Logo Image" className="w-48  object-contain" />
-          <button className="flex items-center space-x-5 rounded-md border border-color-primary bg-white px-9 py-2 text-black group">
+          <button className="group flex items-center space-x-5 rounded-md border border-color-primary bg-white px-9 py-2 text-black">
             <div>
               <Icon
                 icon="ion:paper-plane"
-                className="h-8 w-8 text-color-primary group-hover:rotate-[360deg] transition-all duration-300 ease-in-out"
+                className="h-8 w-8 text-color-primary transition-all duration-300 ease-in-out group-hover:rotate-[360deg]"
               />
             </div>
             <div className="flex flex-col items-start">
@@ -36,25 +36,39 @@ const UserDashboardNav = () => {
               </p>
               <p className="font-bold text-black">0$</p>
             </div>
-            <button className="flex items-center space-x-2 rounded-2xl bg-color-accent px-4 py-2 text-black hover:text-white relative group overflow-hidden">
-              <span className="absolute left-0 top-0 mt-16 h-20 w-full bg-color-decor_orange transition-all duration-300 ease-in-out rounded-3xl group-hover:-mt-4"></span>
+            <button className="group relative flex items-center space-x-2 overflow-hidden rounded-2xl bg-color-accent px-4 py-2 text-black hover:text-white">
+              <span className="absolute left-0 top-0 mt-16 h-20 w-full rounded-3xl bg-color-decor_orange transition-all duration-300 ease-in-out group-hover:-mt-4"></span>
               <div>
-                <CreditCardIcon className="h-6 w-6 text-black relative group-hover:rotate-45 group-hover:text-color-white" />
+                <CreditCardIcon className="relative h-6 w-6 text-black group-hover:rotate-45 group-hover:text-color-white" />
               </div>
-              <div className="flex flex-col items-start relative">
+              <div className="relative flex flex-col items-start">
                 <p className="hidden text-sm font-bold lg:inline-block">
                   Top up
                 </p>
               </div>
             </button>
           </div>
-          <div className="flex items-center justify-between group relative">
+          <div className="group relative flex items-center justify-between">
             <UserCircleIcon className="h-8 w-8 text-color-primary" />
             <ChevronDownIcon className="h-6 w-6 text-color-primary" />
-            <div className="absolute cursor-pointer top-1 right-1/2 bg-color-white text-color-black w-60 h-32 p-4 hidden group-hover:block">
-              <p className="text-color-table_gray text-sm">aim4greatness@gmail.com</p>
-              <Link href="/user/profile" className="flex items-center justify-start hover:bg-color-bg_primary-500 rounded-xl px-4 py-2"><UserIcon width={16} className="text-color-primary mr-2" /> Profile</Link>
-              <Link href="/" className="flex items-center justify-start hover:bg-color-bg_primary-500 rounded-xl px-4 py-2"><PowerIcon width={16} className="text-color-primary mr-2" /> Log Out</Link>
+            <div className="absolute right-1/2 top-14 z-50 hidden h-32 w-60 cursor-pointer bg-color-white p-4 text-color-black shadow-md group-hover:block">
+              <p className="text-sm text-color-table_gray">
+                aim4greatness@gmail.com
+              </p>
+              <Link
+                href="/user/profile"
+                className="flex items-center justify-start rounded-xl px-4 py-2 hover:bg-color-bg_primary-500"
+              >
+                <UserIcon width={16} className="mr-2 text-color-primary" />{" "}
+                Profile
+              </Link>
+              <Link
+                href="/"
+                className="flex items-center justify-start rounded-xl px-4 py-2 hover:bg-color-bg_primary-500"
+              >
+                <PowerIcon width={16} className="mr-2 text-color-primary" /> Log
+                Out
+              </Link>
             </div>
           </div>
         </div>
