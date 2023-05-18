3f2fc41a8005a94413f2a89fee91a9b8e66a3741
Godwin <67944336+UgbeGodwinIkpe@users.noreply.github.com>
2023-05-18 13:39:29 +0100

    again

--- routes/users.js
+++ routes/users.js
@@ -657,7 +657,7 @@ router.get("/blog/post", (req, res, next) => {
     let postid = req.query.post_id;
     try {
         db.query(
-            `SELECT blog_posts.id AS post_id, blog_posts.content AS content FROM blog_posts WHRE blog_posts.id='${postid}' AND JOIN comments ON blog_posts.id=comments.postid`,
+            `SELECT * FROM blog_posts, comments WHRE blog_posts.id='${postid}' AND comments.postid='${postid}'`,
             (err, result) => {
                 // if query error
                 if (err) {
