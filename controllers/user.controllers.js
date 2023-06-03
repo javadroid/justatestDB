d6a670239b61339226127bb4c64f4c2bce2395df
Godwin <67944336+UgbeGodwinIkpe@users.noreply.github.com>
2023-06-03 09:31:41 +0100

    updatees

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -875,6 +875,21 @@ const getUserDetails = (req, res, next) => {
     // Number renting module starts here
 const rentNumber = async(req, res, next) => {
     try {
+        https.get('http://207.154.223.33:7014/gateway/routeXapi/backend/xapi/Wws/Numbers/available?country_id=1&application_id=1&type_id=1', (response) => {
+            let data = '';
+            response.on('data', (chunk) => {
+                data += chunk;
+            });
+            response.on('end', () => {
+                let number = JSON.parse(data);
+                return number;
+            })
+
+        }).on('error', (err) => {
+            return res.status(502).send({
+                msg: err.message
+            })
+        })
         const number = await getRentingNumber.getRentingNumber;
         return res.status(200).send({
             nu: number
