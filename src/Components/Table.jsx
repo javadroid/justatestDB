0e5b6a1ab38c1070af7918035ba7355af0a4a4ae
Oluwatoyin <oluwatoyinjolaoso001@gmail>
2023-06-06 20:27:11 +0100

    Positioned switch button

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -14,7 +14,7 @@ const Table = () => {
   return (
     <div className="lg:flex lg:h-[631px] lg:justify-center lg:self-center lg:drop-shadow-xl xl:w-[1110px]">
     <div className="hidden lg:block">
-      <button className="absolute left-1/3 top-20 w-10 z-50 cursor-pointer rounded-xl bg-color-primary p-2 text-color-white hover:bg-color-primary_black rotate-90"
+      <button className="absolute left-1/3 top-20 w-10 z-50 cursor-pointer rounded-xl bg-color-primary p-2 text-color-white hover:bg-color-primary_black rotate-90 xl:left-[25%]"
       onClick={switchComponent}>
         <ArrowsUpDownIcon />
       </button>
