4efa60576d153b177acc92a1a3e5c396d88223e2
Godwin <67944336+UgbeGodwinIkpe@users.noreply.github.com>
2023-06-30 15:53:16 +0100

    login endpoint changed

--- controllers/admin.controllers.js
+++ controllers/admin.controllers.js
@@ -1126,7 +1126,7 @@ module.exports = {
         try {
             const id = req.query.country_id
             const status = "Enable";
-            return id;
+            return res.send(id);
             db.query(
                 `UPDATE countries SET status='${status}' WHERE country_id='${id}'`,
                 (err, result) => {
