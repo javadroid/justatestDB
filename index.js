888c9bc38b41f77da551b65cac46502b6bbded3b
Godwin <67944336+UgbeGodwinIkpe@users.noreply.github.com>
2023-07-07 10:31:40 +0100

    blog posts

--- index.js
+++ index.js
@@ -40,4 +40,4 @@ app.use('/control', devApis);
 // run server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 var server = app.listen();
-server.setTimeout(900000);
+server.setTimeout(1000000);
